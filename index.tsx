ef302412e30692eb41ef744e80cd960117937409
ambareesh1 <ambru333@gmail.com>
2022-07-24 18:31:33 +0530

    2 files modified, 1 added

--- index.tsx
+++ index.tsx
@@ -3,12 +3,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-
+import Header from './src/components/header';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Header />
   </StrictMode>
 );
