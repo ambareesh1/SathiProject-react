028408aecbc7afbe304f8fd237e794358eda4012
ambareesh1 <ambru333@gmail.com>
2022-07-24 23:03:20 +0530

    3 files modified

--- index.tsx
+++ index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './src/assets/css/style.css';
+import './src/assets/css/custom.css';
+import './src/assets/css/vendor.min.css';
 
-import App from './App';
 import Header from './src/components/header';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
